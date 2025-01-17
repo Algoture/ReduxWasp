ca3935e3ab489aaefa1dbeb85e6daa5c52826280
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-17 19:54:55 +0530

    tailwind and react redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import "./App.css";
-
 function App() {
-
-  return <>h1</>;
+  return (
+    <>
+      <h1 className="border">React App</h1>
+      <p>Ready to start coding!</p>
+    </>
+  );
 }
 
 export default App;
